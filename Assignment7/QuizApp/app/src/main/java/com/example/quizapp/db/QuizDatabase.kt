fbbbe4201ebde7f60a6660b0acb5b1a2205cd003
wineikhaing <wineikhaing111111@gmail.com>
2023-01-15 15:44:43 -0500

    completed Assignment 7 Quiz App

---
+++ Assignment7/QuizApp/app/src/main/java/com/example/quizapp/db/QuizDatabase.kt
@@ -0,0 +1,34 @@
+package com.example.quizapp.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [Quiz::class],
+    version = 1,
+    exportSchema = true
+)
+abstract class QuizDatabase : RoomDatabase() {
+    abstract fun getQuizDao(): QuizDao
+
+    companion object {
+        @Volatile
+        private var instance: QuizDatabase? = null
+        private val LOCK = Any()
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: buildDatabase(context).also {
+                instance = it
+            }
+        }
+
+        private fun buildDatabase(context: Context): QuizDatabase {
+            return Room.databaseBuilder(
+                context.applicationContext,
+                QuizDatabase::class.java,
+                "quizdatabase"
+            ).build()
+        }
+    }
+}
